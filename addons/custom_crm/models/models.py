a9b0c75bdb226a8130b17c9889db5d5f368f2bf3
Jose Noguera <josenoguera1994@gmail.com>
2022-09-03 01:11:36 -0500

    nuevo modelo y su vista formulario y arbol

--- addons/custom_crm/models/models.py
+++ addons/custom_crm/models/models.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+
+class visit(models.Model):
+    _name = 'custom_crm.visit' # Model name
+    _description = 'Visit' # Model description
+
+    name = fields.Char(string='Descripción') # Field name suele pintarse en el formulario en el encabezado de la vista
+    customer = fields.Char(string='Cliente')
+    date = fields.Datetime(string='Fecha')
+    type = fields.Selection([('P', 'Presencial'), ('W', 'WhatsApp'), ('T', 'Telefónico')], string='Tipo', required=True)
+    done = fields.Boolean(string='Realizada', readonly=True)
+
+
 
 
 # class custom_crm(models.Model):
