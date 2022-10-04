85e82b59137289603de5eb54b3d9f708332c9e12
Jose Noguera <josenoguera1994@gmail.com>
2022-10-03 23:49:30 -0500

    Crud con orm de odoo

--- addons/custom_crm/models/models.py
+++ addons/custom_crm/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+import datetime
 
 
 class visit(models.Model):
@@ -16,6 +17,31 @@ class visit(models.Model):
     def button_toggle_state(self):
         self.done = not self.done
 
+    def f_create(self):
+        """ Create a new record """
+        visit = {
+            "name": "ORM test",
+            "customer": 1,
+            "date": str(datetime.date(2020, 8, 6)),
+            "type": "P",
+            "done": False
+        }
+        print(visit)
+        self.env['custom_crm.visit'].create(visit) # self.env['custom_crm.visit'] es un metodo que crea un objeto de la clase visit
+    
+    def f_search_update(self):
+        """ Search and update a record """
+        visit = self.env['custom_crm.visit'].search([('name', '=', 'ORM test')]) # pasa una lista de tuplas con los criterios de busqueda
+        # print("search: ", visit, visit.name)
+        visit_bis = self.env['custom_crm.visit'].browse([8]) # browse es para buscar por id
+
+        visit.write({'name': 'ORM test updated'}) # update
+
+    def f_delete(self):
+        """ Delete a record """
+        visit = self.env['custom_crm.visit'].search([('name', '=', 'ORM test updated')])
+        # visit = self.env['custom_crm.visit'].browse([8])
+        visit.unlink()
 
 # class custom_crm(models.Model):
 #     _name = 'custom_crm.custom_crm'
